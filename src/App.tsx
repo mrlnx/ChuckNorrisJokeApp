3f88cc72330502f39fa194c500c683ddd0e6757c
Marlon Lede <marlonlede@gmail.com>
2020-06-29 16:00:28 +0200

    Added the fetch button

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,36 @@ const App: React.RC = () => {
      * We want an Application where we can fetch 10 Random Chuck Norris jokes. These jokes can be fetched from the following API http://api.icndb.com/jokes/random/10.
      */
 
+    const [fetchJokes, setFetchJokes] = useState<{jokesFetched: boolean}>(false);
     const [jokes, setJokes] = useState<IJokes>([]);
+
+    /**
+     * Fetch Jokes Button
+     * When these jokes are fetched via a button they need to be displayed in a list. In this list we can mark certain jokes as favorite.
+     */
+
+    const handleFetchJokes = () => setFetchJokes(true);
 
     /**
      * Get Jokes Effect
      */
 
     useEffect(() => {
-        console.log("fetch jokes");
-
-        fetch("http://api.icndb.com/jokes/random/10.", {})
-            .then((res) => res.json())
-            .then((data) => setJokes(data.value))
-            .catch((err) => console.error(err));
-    }, []);
+        if (fetchJokes) {
+            console.log("fetch jokes");
+            fetch("http://api.icndb.com/jokes/random/10.", {})
+                .then((res) => res.json())
+                .then((data) => setJokes(data.value))
+                .catch((err) => console.error(err));
+            setFetchJokes(false);
+        }
+    }, [fetchJokes]);
 
     return (
         <>
             <>
                 <h1>Chuck Norris Jokes App</h1>
+                <button onClick={() => handleFetchJokes()}>Fetch Jokes</button>
             </>
             <>
                 <h2>Jokes</h2>
